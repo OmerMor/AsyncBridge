b17f4c0f08526bb812bf2ccc74ab82c136213924
Alex Davies <alexdavies74@gmail.com>
2012-04-09 22:43:20 +0100

    Add TaskUtils.Delay and a test for it

---
+++ AsyncBridge.Tests/DelayTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsyncBridge.Tests
+{
+    [TestClass]
+    public class DelayTest
+    {
+        [TestMethod]
+        public async Task CheckItDoesReturn()
+        {
+            await TaskUtils.Delay(0);
+        }
+    }
+}
