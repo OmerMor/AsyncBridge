77e11db50b0ef60642b9f051e365b132f2a4e25b
Omer Mor <omer.mor@gmail.com>
2012-04-10 01:32:56 +0300

    improved task.delay

--- AsyncBridge/TaskUtils.cs
+++ AsyncBridge/TaskUtils.cs
@@ -1,22 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncBridge
 {
-    public class TaskUtils
+    public static class TaskUtils
     {
-        public static Task Delay(int millis)
+        public static Task Delay(TimeSpan delay)
         {
-            // There isn't a non-generic version of this. We should have a unit type
-            TaskCompletionSource<object> taskCompletionSource = new TaskCompletionSource<object>();
-
-            Timer timer = new Timer(_ => taskCompletionSource.SetResult(null), null, millis, Timeout.Infinite);
-
-            return taskCompletionSource.Task;
+            return Delay(delay, CancellationToken.None);
         }
+
+        public static Task Delay(TimeSpan delay, CancellationToken cancellationToken)
+        {
+            var num = (long) delay.TotalMilliseconds;
+            if (num < -1L || num > int.MaxValue)
+                throw new ArgumentOutOfRangeException("delay");
+
+            return Delay((int) num, cancellationToken);
+        }
+
+        public static Task Delay(int millisecondsDelay)
+        {
+            return Delay(millisecondsDelay, CancellationToken.None);
+        }
+
+        public static Task Delay(int millisecondsDelay, CancellationToken cancellationToken)
+        {
+            if (millisecondsDelay < Timeout.Infinite)
+                throw new ArgumentOutOfRangeException("millisecondsDelay");
+
+            if (cancellationToken.IsCancellationRequested)
+                return fromCancellation(cancellationToken);
+
+            if (millisecondsDelay == 0)
+                return s_completedTask.Value;
+
+            var delayPromise = new DelayPromise(cancellationToken);
+            if (cancellationToken.CanBeCanceled)
+                delayPromise.Registration = cancellationToken.Register(state => ((DelayPromise) state).Complete(),
+                                                                         delayPromise, useSynchronizationContext: false);
+            if (millisecondsDelay != Timeout.Infinite)
+            {
+                delayPromise.Timer = new Timer(state => ((DelayPromise) state).Complete(), delayPromise,
+                                                 millisecondsDelay, Timeout.Infinite);
+                GC.SuppressFinalize(delayPromise.Timer);
+            }
+            return delayPromise.TaskCompletionSource.Task;
+        }
+
+        private sealed class DelayPromise
+        {
+            internal CancellationTokenRegistration Registration;
+            internal Timer Timer;
+            internal readonly TaskCompletionSource<object> TaskCompletionSource = new TaskCompletionSource<object>();
+            private readonly CancellationToken m_token;
+
+            internal DelayPromise(CancellationToken token)
+                //: base((object)null, CancellationToken.None, TaskCreationOptions.None, InternalTaskOptions.PromiseTask)
+            {
+                m_token = token;
+            }
+
+            internal void Complete()
+            {
+                var alreadyDisposed =
+                    !(m_token.IsCancellationRequested ? TaskCompletionSource.TrySetCanceled() : TaskCompletionSource.TrySetResult(null));
+                if (alreadyDisposed)
+                    return;
+                if (Timer != null)
+                    Timer.Dispose();
+                Registration.Dispose();
+            }
+        }
+
+        private static Task fromCancellation(CancellationToken cancellationToken)
+        {
+            if (!cancellationToken.IsCancellationRequested)
+                throw new ArgumentOutOfRangeException("cancellationToken");
+            var tcs = new TaskCompletionSource<object>();
+            tcs.SetCanceled();
+            return tcs.Task;
+        }
+
+        private static readonly Lazy<Task> s_completedTask = new Lazy<Task>(() =>
+        {
+            var tcs = new TaskCompletionSource<object>();
+            tcs.SetResult(null);
+            return tcs.Task;
+        }, isThreadSafe: true);
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ AsyncBridge provides new C#5 async features support for .NET4 projects.
 
 Based on [gist](https://gist.github.com/1961087) by Daniel Grunwald (from SharpDevelop).
 
+Thanks goes to Alex Davies for the [idea](http://www.simple-talk.com/community/blogs/alex/archive/2012/04/08/107197.aspx).
