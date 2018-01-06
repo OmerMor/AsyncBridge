9c7132c217e40db597f2f8652f7f312eff3cf8fc
jnm2 <me@jnm2.com>
2018-01-06 17:43:15 -0500

    Fixed build warnings

--- src/AsyncBridge/Runtime.CompilerServices/CallerLineNumberAttribute.cs
+++ src/AsyncBridge/Runtime.CompilerServices/CallerLineNumberAttribute.cs
@@ -3,14 +3,12 @@ namespace System.Runtime.CompilerServices
     /// <summary>
     /// Allows you to obtain the line number in the source file at which the method is called.
     /// </summary>
-    /// <remarks>
-    /// You apply the <b>CallerFilePath</b> attribute to an optional parameter that has a default value. 
-    /// You must specify an explicit default value for the optional parameter.
-    /// You can't apply this attribute to parameters that aren't specified as optional.
-    /// </remarks>
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
     public sealed class CallerLineNumberAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallerLineNumberAttribute"/> class.
+        /// </summary>
         [TargetedPatchingOptOut("Performance critical to inline this type of method across NGen image boundaries")]
         public CallerLineNumberAttribute()
         {
