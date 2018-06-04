0dcaeb5f9677d7d904532a31c185e518e872df9d
jnm2 <me@jnm2.com>
2018-06-03 21:02:29 -0400

    Document AsyncBridgeExtensions itself

---
+++ src/AsyncBridge/AsyncBridgeExtensions.cs
@@ -0,0 +1,9 @@
+#if NET40 || PORTABLE
+/// <summary>
+/// Contains extension methods which would be instance methods if it was possible
+/// for AsyncBridge to modify existing BCL types for the current target framework.
+/// </summary>
+public static partial class AsyncBridgeExtensions
+{
+}
+#endif
